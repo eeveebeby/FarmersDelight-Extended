5479bbeddf28caefa8df5afd806427fbe6c2f7b1
opee2012 <jborth2010@gmail.com>
2025-07-26 06:45:17 -0400

    Fixed dependencies and fixing recipe

--- src/main/java/com/eeveebeby/farmersdelight_extended/FarmersDelight_Extended.java
+++ src/main/java/com/eeveebeby/farmersdelight_extended/FarmersDelight_Extended.java
@@ -2,6 +2,9 @@ package com.eeveebeby.farmersdelight_extended;
 
 import com.eeveebeby.farmersdelight_extended.item.ModCreativeModeTabs;
 import com.eeveebeby.farmersdelight_extended.item.ModItems;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -46,18 +49,26 @@ public class FarmersDelight_Extended {
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    private void commonSetup(FMLCommonSetupEvent event) {
+    private void commonSetup(final FMLCommonSetupEvent event) {
 
     }
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
 
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
     }
 }
